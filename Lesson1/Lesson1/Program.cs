71da882ee5967ec5f7c829919fdac4899c571bf2
shabanov1903 <61244002+shabanov1903@users.noreply.github.com>
2021-04-26 22:08:35 +0300

    lesson-1 (#1)
    merge lesson-1 with main

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Приветсвтенная строка программы
+            Console.WriteLine("Введите имя и нажмите Enter");
+            // Ожидание ввода имени пользователя
+            var Name = Console.ReadLine();
+            // Вывод шаблона строки с текущей датой (без времени)
+            Console.WriteLine($"Привет, {Name}, сегодня {DateTime.Now.ToShortDateString()}!");
+        }
+    }
+}
