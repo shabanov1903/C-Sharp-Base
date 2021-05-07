ef9289ec3049e3467e8ea4dccc13a63e962eb15e
shabanov1903 <61244002+shabanov1903@users.noreply.github.com>
2021-05-07 21:36:20 +0300

    lesson-3 (#3)
    merge lesson-3 with main

---
+++ Lesson3/L3-5/L3-5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace L3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пременная для запоминания элемента n-2
+            int fSumma1 = 0;
+            // Пременная для запоминания элемента n-1
+            int fSumma2 = 0;
+            Console.WriteLine("Введите порядковый номер элемента ряда Фибоначчи:");
+            int fNumber = Convert.ToInt32(Console.ReadLine());
+            // Если пользователь ввёл число больше 0, то задаются начальные условия
+            if (fNumber > 0) fSumma2 = 1;
+            for (int i = 0; i < fNumber; i++)
+            {
+                fSumma2 = fSumma1 + fSumma2;
+                // В случае нулевой итерации fSumma1 должна остаться нулевой
+                if (i != 0) fSumma1 = fSumma2 - fSumma1;
+            }
+            Console.WriteLine($"Значение элемента ряда (через цикл): {fSumma2}");
+            Console.WriteLine($"Значение элемента ряда (через рекурсию): {fSum(fNumber)}");
+        }
+
+        static int fSum(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fSum(n - 1) + fSum(n - 2);
+            }
+        }
+    }
+}
