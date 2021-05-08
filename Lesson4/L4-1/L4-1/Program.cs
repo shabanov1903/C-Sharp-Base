2d6305d78eba4155a3adb9a767170616c37afd82
Danil Shabanov <shabanov1903@gmail.com>
2021-05-08 15:50:16 +0300

    lesson-4

---
+++ Lesson4/L4-1/L4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] fullName = new string[4, 3];
+            fullName[0, 0] = "Абросимов";   fullName[0, 1] = "Виктор";      fullName[0, 2] = "Витальевич";
+            fullName[1, 0] = "Сельков";     fullName[1, 1] = "Алексей";     fullName[1, 2] = "Семёнович";
+            fullName[2, 0] = "Балыбердин";  fullName[2, 1] = "Владимир";    fullName[2, 2] = "Борисвич";
+            fullName[3, 0] = "Семёнов";     fullName[3, 1] = "Сергей";      fullName[3, 2] = "Геннадьевич";
+
+            for (int i = 0; i < fullName.GetLength(0); i++)
+            {
+                Console.WriteLine($"Имя пользователя {i + 1}:");
+                Console.WriteLine(GetFullName(fullName[i, 0], fullName[i, 1], fullName[i, 2]));
+            }
+        }
+
+        private static string GetFullName (string firstName, string secondName, string lastName)
+        {
+            return String.Join(" ", firstName, secondName, lastName);
+        }
+    }
+}
---
+++ Lesson4/L4-3/L4-3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace L4_3
+{
+    class Program
+    {
+        enum Year
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
+        // Переменная-индикатор ошибки
+        static bool error = true;
+        static void Main(string[] args)
+        {
+            // Номер месяца
+            int number;
+            
+            // Цикл будет предлагать ввести номер месяца, пока он не будет введён корректно
+            while (error)
+            {
+                Console.WriteLine("Введите порядковый номер месяца:");
+                number = Int32.Parse(Console.ReadLine());
+                // Вызов функции и запись в переменную соответствующего emun
+                Year year = Season(number);
+                if (error)
+                {
+                    Console.WriteLine("Ошибка: введите число от 1 до 12:");
+                }
+                else
+                {
+                    // Вывод на консоль времени года и завершение программы
+                    Console.WriteLine("Время года:");
+                    Console.WriteLine(SeasonDetect(year));
+                }
+            }
+        }
+
+        private static Year Season (int number)
+        {
+            // В случае удачного завершения программы флаг останется в значении false
+            error = false;
+            switch (number)
+            {
+                case 1: return Year.Winter;
+                case 2: return Year.Winter;
+                case 3: return Year.Spring;
+                case 4: return Year.Spring;
+                case 5: return Year.Spring;
+                case 6: return Year.Summer;
+                case 7: return Year.Summer;
+                case 8: return Year.Summer;
+                case 9: return Year.Autumn;
+                case 10: return Year.Autumn;
+                case 11: return Year.Autumn;
+                case 12: return Year.Winter;
+            }
+            // При правильно введённом значении месяца программа не дойдёт до этого места 
+            error = true;
+            // Дефолтное значение (не используется в программе)
+            return Year.Winter;
+        }
+        private static string SeasonDetect(Year year)
+        {
+            switch (year)
+            {
+                case Year.Winter: return "Зима";
+                case Year.Spring: return "Весна";
+                case Year.Summer: return "Лето";
+                case Year.Autumn: return "Осень";
+                // Дефолтное значение (не используется в программе)
+                default: return "";
+            }
+        }
+    }
+}
