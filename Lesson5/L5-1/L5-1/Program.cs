cea06086b5ed65d50785fc473d2099c73b44daaf
Danil Shabanov <shabanov1903@gmail.com>
2021-05-12 19:00:04 +0300

    lesson-5

---
+++ Lesson5/L5-1/L5-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace L5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Относительный путь на директорию с файлом "Program.cs"
+            var workDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"));
+            Console.WriteLine("Введите любой текст и нажмите ENTER. Сохранённый текст можно найти по следующему пути:");
+            Console.WriteLine(workDir);
+            var inputText = Console.ReadLine();
+            // Путь к создаваемому файлу "L5_1_Text.txt"
+            var path = Path.Combine(workDir, "L5_1_Text.txt");
+            // Создание и запись в файл "L5_1_Text.txt"
+            File.WriteAllText(path, inputText);
+        }
+    }
+}
---
+++ Lesson5/L5-2/L5-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace L5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Относительный путь на директорию с файлом "Program.cs"
+            var workDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"));
+            // Путь к создаваемому/дополняемому файлу "startup.txt"
+            var path = Path.Combine(workDir, "startup.txt");
+            // Если файл существует, то запись с переходом на новую строчку
+            if (File.Exists(path) == false) File.AppendAllText(path, DateTime.Now.ToString());
+            else File.AppendAllText(path, Environment.NewLine + DateTime.Now.ToString());
+        }
+    }
+}
