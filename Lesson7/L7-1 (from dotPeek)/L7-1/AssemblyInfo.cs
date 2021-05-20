4a8d2a251f75812f54e4e6a43afb2534d8c34df9
Danil Shabanov <shabanov1903@gmail.com>
2021-05-20 19:37:03 +0300

    lesson-7

---
+++ Lesson7/L7-1 (from dotPeek)/L7-1/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("L7-1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("L7-1")]
+[assembly: AssemblyTitle("L7-1")]
+[assembly: AssemblyVersion("1.0.0.0")]
