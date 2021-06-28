8645441fc2ad5f5ee7284cbdc7618fc3f52984d5
shabanov1903 <61244002+shabanov1903@users.noreply.github.com>
2021-06-28 20:41:32 +0300

    lesson-8 (#8)

---
+++ Lesson8/L8-1/L8-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace L8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Settings1.Default.Hello);
+            if (string.IsNullOrEmpty(Properties.Settings1.Default.Name) == false)
+            {
+                Console.WriteLine($"Имя предыдущего пользователя: {Properties.Settings1.Default.Name}");
+            }
+            if (string.IsNullOrEmpty(Properties.Settings1.Default.Age) == false)
+            {
+                Console.WriteLine($"Возраст предыдущего пользователя: {Properties.Settings1.Default.Age}");
+            }
+            if (string.IsNullOrEmpty(Properties.Settings1.Default.Job) == false)
+            {
+                Console.WriteLine($"Профессия предыдущего пользователя: {Properties.Settings1.Default.Job}");
+            }
+            Console.WriteLine("Введите имя пользователя, которое отобразится при следующем входе в приложение:");
+            Properties.Settings1.Default.Name = Console.ReadLine();
+            Console.WriteLine("Введите возраст пользователя, который отобразится при следующем входе в приложение:");
+            Properties.Settings1.Default.Age = Console.ReadLine();
+            Console.WriteLine("Введите профессию пользователя, которая отобразится при следующем входе в приложение:");
+            Properties.Settings1.Default.Job = Console.ReadLine();
+            Properties.Settings1.Default.Save();
+        }
+    }
+}
